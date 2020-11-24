d317040a06a30c63bec48ed2d6eb93c3df0c41e6
Damian Kucharski <kuchar187@gmail.com>
2020-11-24 18:19:01 +0100

    Add project files.

---
+++ MyTrips/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MyTrips.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyTrips.Models.DbModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyTrips.Models.DbModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
